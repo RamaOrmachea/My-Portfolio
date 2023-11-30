fde2c4870e5b42e46bde34d2b9828d99de982daa
StonyKing <ramiro_hero@hotmail.com>
2023-11-30 15:23:26 -0300

    Final touches 2.0

--- scripts/carrousel.js
+++ scripts/carrousel.js
@@ -1,46 +1,67 @@
 document.querySelectorAll(".carousel").forEach((carousel) => {
-  const items = carousel.querySelectorAll(".carousel__item");
-  const buttonsHtml = Array.from(items, () => {
-      return `<span class="carousel__button"></span>`;
-  });
+    const items = carousel.querySelectorAll(".carousel__item");
+    const buttonsHtml = Array.from(items, () => {
+        return `<span class="carousel__button"></span>`;
+    });
 
-  carousel.insertAdjacentHTML(
-      "beforeend",
-      `
-      <div class="carousel__nav">
-          ${buttonsHtml.join("")}
-      </div>
-      `
-  );
+    carousel.insertAdjacentHTML(
+        "beforeend",
+        `
+        <div class="carousel__nav">
+            ${buttonsHtml.join("")}
+        </div>
+        `
+    );
 
-  const buttons = carousel.querySelectorAll(".carousel__button");
+    const buttons = carousel.querySelectorAll(".carousel__button");
 
-  let currentIndex = 0; // Track the current index of the selected item
+    let currentIndex = 0;
 
-  const changeItem = (index) => {
-      items.forEach((item) =>
-          item.classList.remove("carousel__item--selected")
-      );
-      buttons.forEach((button) =>
-          button.classList.remove("carousel__button--selected")
-      );
+    const changeItem = (index) => {
+        items.forEach((item) =>
+            item.classList.remove("carousel__item--selected")
+        );
+        buttons.forEach((button) =>
+            button.classList.remove("carousel__button--selected")
+        );
 
-      items[index].classList.add("carousel__item--selected");
-      buttons[index].classList.add("carousel__button--selected");
-  };
+        items[index].classList.add("carousel__item--selected");
+        buttons[index].classList.add("carousel__button--selected");
+    };
 
-  buttons.forEach((button, i) => {
-      button.addEventListener("click", () => {
-          currentIndex = i;
-          changeItem(currentIndex);
-      });
-  });
+    buttons.forEach((button, i) => {
+        button.addEventListener("click", () => {
+            currentIndex = i;
+            changeItem(currentIndex);
+        });
+    });
 
-  setInterval(() => {
-      currentIndex = (currentIndex + 1) % items.length;
-      changeItem(currentIndex);
-  }, 5000);
+    
+    const observer = new IntersectionObserver(
+        (entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    
+                    const intervalId = setInterval(() => {
+                        currentIndex = (currentIndex + 1) % items.length;
+                        changeItem(currentIndex);
+                    }, 5000);
 
-  items[0].classList.add("carousel__item--selected");
-  buttons[0].classList.add("carousel__button--selected");
+                    
+                    carousel.carouselIntervalId = intervalId;
+                } else {
+                    
+                    clearInterval(carousel.carouselIntervalId);
+                }
+            });
+        },
+        { threshold: 0.5 } 
+    );
+
+    
+    observer.observe(carousel);
+
+    
+    items[0].classList.add("carousel__item--selected");
+    buttons[0].classList.add("carousel__button--selected");
 });
